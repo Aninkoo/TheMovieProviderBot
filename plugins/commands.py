101915204f864f27198be3d33ea74867178fa7a3
Aninkoo <112639445+Aninkoo@users.noreply.github.com>
2024-03-20 02:22:12 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -302,7 +302,7 @@ async def start(client, message):
         if is_valid == True:
             await message.reply_text(
                 text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all movies till today midnight.</b>",
-                protect_content=True
+                protect_content=False
             )
             await verify_user(client, userid, token)
         else:
